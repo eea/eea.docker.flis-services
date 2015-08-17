aee077e8354cdea1013e407d0934b85ebf09f41a
Andreea Dima <andreeadima95@gmail.com>
2015-08-17 10:33:45 +0300

    [docker] Updated frontend image

--- frontend/config/run.sh
+++ frontend/config/run.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 #Update default.conf with env variables
-sed -i s/{{SERVER_NAME}}/$SERVER_NAME/ nginx.conf
-sed -i s/{{PREFIX}}/$PREFIX/ nginx.conf
-cp nginx.conf /etc/nginx/conf.d/default.conf
+sed -i s/{{SERVER_NAME}}/$SERVER_NAME/ /etc/nginx/conf.d/default.conf
+sed -i s/{{PREFIX}}/$PREFIX/ /etc/nginx/conf.d/default.conf
 
 #Run nginx server
 cd /etc/nginx
